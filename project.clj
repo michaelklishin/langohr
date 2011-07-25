2d74d9e13ff0f59160e29c5fe1d9d27d4755f0a3
Michael S. Klishin <michael@novemberain.com>
2011-07-25 09:25:09 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; See https://github.com/technomancy/leiningen/blob/stable/sample.project.clj
 ;; to learn more about available options.
-(defproject leporidae "0.1.0-SNAPSHOT"
+(defproject langohr "0.1.0-SNAPSHOT"
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
   :dependencies [[org.clojure/clojure "1.3.0-BETA1"]
                  [com.rabbitmq/amqp-client "2.5.1"]]
