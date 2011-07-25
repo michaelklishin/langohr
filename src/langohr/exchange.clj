18f3b1dd1398bbaa398c3df5e804fb89891c25e4
Michael S. Klishin <michael@novemberain.com>
2011-07-25 06:02:18 +0400

    Implement langohr.exchange.declare

---
+++ src/langohr/exchange.clj
@@ -0,0 +1,13 @@
+(ns langohr.exchange
+  (:import (com.rabbitmq.client Channel)))
+
+;;
+;; API
+;;
+
+(defn declare
+  "Declares an exchange using exchange.declare AMQP method"
+  ([^Channel channel ^String name ^String type]
+     (.exchangeDeclare channel name type))
+  ([^Channel channel ^String name ^String type { :keys [durable auto-delete internal arguments] :or {durable false, auto-delete false, internal false} }]
+     (.exchangeDeclare channel name type durable auto-delete internal arguments)))
