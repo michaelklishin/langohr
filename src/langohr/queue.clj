b0c51957180aa2df7b4e463be00bfd93e33c7545
Michael S. Klishin <michael@novemberain.com>
2011-07-26 08:28:42 +0400

    Introduce langohr.queue/bind

--- src/langohr/queue.clj
+++ src/langohr/queue.clj
@@ -11,3 +11,14 @@
      (.queueDeclare channel))
   ([^Channel channel ^String queue {:keys [durable auto-delete exclusive arguments] :or {durable false, auto-delete true, exclusive true}}]
      (.queueDeclare channel queue durable exclusive auto-delete arguments)))
+
+
+(defn bind
+  "Binds a queue to an exchange using queue.bind AMQP method"
+  ([^Channel channel ^String queue ^String exchange]
+     (.queueBind channel queue exchange ""))
+  ([^Channel channel ^String queue ^String exchange { :keys [routing-key arguments] :or { routing-key "", arguments nil } }]
+     (.queueBind channel queue exchange routing-key arguments)))
+
+
+
