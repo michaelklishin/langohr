09b73801570891917828d547a1b60e2b80e6e246
Michael S. Klishin <michael@novemberain.com>
2011-07-26 10:20:26 +0400

    Reuse AMQP connection & channel between queue.declare tests

--- test/langohr/test/queue.clj
+++ test/langohr/test/queue.clj
@@ -2,12 +2,19 @@
   (:import (com.rabbitmq.client Channel AMQP AMQP$Queue$DeclareOk))
   (:use [clojure.test] [langohr.core :as lhc] [langohr.queue :as lhq]))
 
+
+;;
+;; queue.declare
+;;
+
+(defonce *conn*    (lhc/connect))
+(defonce *channel* (lhc/create-channel *conn*))
+
+
 (deftest t-declare-a-server-named-queue-with-default-attributes
-  (let [conn       (lhc/connect)
-        ch         (lhc/create-channel conn)
-        declare-ok (lhq/declare ch)]
-    (is (lhc/open? conn))
-    (is (lhc/open? ch))
+  (let [declare-ok (lhq/declare *channel*)]
+    (is (lhc/open? *conn*))
+    (is (lhc/open? *channel*))
     (is (instance? AMQP$Queue$DeclareOk declare-ok))
     (is (re-seq #"^amq\.gen-(.+)" (.getQueue declare-ok)))
     (is (= 0 (.getConsumerCount declare-ok)))
@@ -15,25 +22,26 @@
 
 
 (deftest t-declare-a-client-named-queue-with-default-attributes
-  (let  [conn       (lhc/connect)
-         channel    (lhc/create-channel conn)
-         queue-name "langohr.tests.queues.client-named-with-default-attributes"
-         declare-ok (lhq/declare channel queue-name)]
+  (let  [queue-name "langohr.tests.queues.client-named-with-default-attributes"
+         declare-ok (lhq/declare *channel* queue-name)]
     (is (= (.getQueue declare-ok) queue-name))))
 
 
 (deftest t-declare-a-non-durable-exclusive-auto-deleted-client-named-queue
-  (let  [conn       (lhc/connect)
-         channel    (lhc/create-channel conn)
-         queue-name "langohr.tests.queues.client-named.non-durable.exclusive.auto-deleted"
-         declare-ok (lhq/declare channel queue-name { :durable false, :exclusive true, :auto-delete true })]
+  (let  [queue-name "langohr.tests.queues.client-named.non-durable.exclusive.auto-deleted"
+         declare-ok (lhq/declare *channel* queue-name { :durable false, :exclusive true, :auto-delete true })]
     (is (= (.getQueue declare-ok) queue-name))))
 
 
 (deftest t-declare-a-durable-non-exclusive-non-auto-deleted-client-named-queue
-  (let  [conn       (lhc/connect)
-         channel    (lhc/create-channel conn)
-         queue-name "langohr.tests.queues.client-named.durable.non-exclusive.non-auto-deleted"
-         declare-ok (lhq/declare channel queue-name { :durable true, :exclusive false, :auto-delete false })]
+  (let  [queue-name "langohr.tests.queues.client-named.durable.non-exclusive.non-auto-deleted"
+         declare-ok (lhq/declare *channel* queue-name { :durable true, :exclusive false, :auto-delete false })]
     (is (= (.getQueue declare-ok) queue-name))))
 
+
+
+;;
+;; queue.bind
+;;
+
+;; TBD
