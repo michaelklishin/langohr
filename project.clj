ca9d1a67ef24521129b97266a0a4140f22ac24b5
Michael S. Klishin <michael@novemberain.com>
2011-07-31 15:28:37 +0400

    Makes dependency resolution works on travis-ci.org

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
 ;; to learn more about available options.
 (defproject langohr "0.1.0-SNAPSHOT"
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
-  :dependencies [[org.clojure/clojure "1.3.0-BETA1"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [com.rabbitmq/amqp-client "2.5.1"]]
   :jvm-opts ["-Xmx512m"])
