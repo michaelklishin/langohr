f78cb0d5c91aa4caa22207ac4793ec01e5db41a4
Michael S. Klishin <michael@novemberain.com>
2011-08-11 01:14:00 +0400

    env isn't necessary

--- test/langohr/test/basic.clj
+++ test/langohr/test/basic.clj
@@ -1,7 +1,8 @@
 (set! *warn-on-reflection* true)
 
 (ns langohr.test.basic
-  (:import (com.rabbitmq.client Connection Channel AMQP AMQP$BasicProperties AMQP$BasicProperties$Builder QueueingConsumer GetResponse))
+  (:import (com.rabbitmq.client Connection Channel AMQP AMQP$BasicProperties AMQP$BasicProperties$Builder QueueingConsumer GetResponse QueueingConsumer$Delivery)
+           (java.security SecureRandom) (java.math.BigInteger))
   (:use [clojure.test] [langohr.core :as lhc] [langohr.queue :as lhq] [langohr.basic :as lhb]))
 
 ;;
@@ -11,16 +12,22 @@
 (defonce ^:dynamic ^Connection *conn* (lhc/connect))
 
 
-(deftest t-publishing-using-default-exchange-and-default-message-attributes
+(deftest t-publishing-using-default-exchange-and-default-message-attributes-take-1
   (let [channel    (.createChannel *conn*)
         exchange   ""
         ;; yes, payload may be blank. This is an edge case Ruby amqp
         ;; gem did not support for a long time so I want to use it in the langohr
         ;; test suite. MK.
         payload    ""
-        queue      "langohr.examples.publishing.using-default-exchange"
-        declare-ok (lhq/declare channel queue { :auto-delete true })]
-    (lhb/publish channel payload { :routing-key queue, :exchange exchange })))
+        queue        "langohr.examples.publishing.using-default-exchange"
+        declare-ok   (lhq/declare channel queue { :auto-delete true })
+        monitor      (Object.)
+        consumer-tag (.toString (new BigInteger 130 (SecureRandom.)) 32)]
+    (do
+      (lhb/publish channel payload { :routing-key queue, :exchange exchange })
+      (lhb/consume channel queue #((.notify monitor)) { :consumer-tag consumer-tag, :auto-ack true })
+      (.wait monitor))))
+
 
 
 ;;
@@ -41,7 +48,7 @@
       (is (= (.. get-response getEnvelope getExchange) exchange))
       (is (= (.. get-response getEnvelope getRoutingKey) queue)))))
 
-(deftest t-basic-get-with-automatic-ack
+(deftest t-basic-get-with-explicit-ack
   (let [channel    (.createChannel *conn*)
         exchange   ""
         payload    "A message we will fetch with basic.get"
