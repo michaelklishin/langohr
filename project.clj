ba3e418be81b8571b0cf1a4eb32185a7861ae058
Michael S. Klishin <michael@novemberain.com>
2011-08-16 16:52:07 +0400

    Correct organization name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; See https://github.com/technomancy/leiningen/blob/stable/sample.project.clj
 ;; to learn more about available options.
-(defproject langohr "0.2.0-SNAPSHOT"
+(defproject com.novemberain/langohr "0.2.0-SNAPSHOT"
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
