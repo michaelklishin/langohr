d9c1c9617e5b49e48fc26e8510a592c8c9cb9f62
Michael S. Klishin <michael@novemberain.com>
2011-08-16 16:27:39 +0400

    Add license information to the project file

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
 ;; to learn more about available options.
 (defproject langohr "0.2.0-SNAPSHOT"
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
+  :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [com.rabbitmq/amqp-client "2.5.1"]]
   :jvm-opts ["-Xmx512m"])
