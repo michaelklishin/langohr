65a000e0db3dd4daa7ed85f30fa63974c7e9f9c5
Michael S. Klishin <michael@novemberain.com>
2011-08-30 01:11:18 +0400

    Add project URL to project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
 ;; to learn more about available options.
 (defproject com.novemberain/langohr "0.2.0-SNAPSHOT"
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
-  :license { :name "Eclipse Public License" }
+  :url         "https://github.com/michaelklishin/langohr"
+  :license     { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta2"]
                  [com.rabbitmq/amqp-client "2.5.1"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]]
