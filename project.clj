4f66ee2d23c263b9c7a7df43af57fc27a2d9082f
Michael S. Klishin <michael@novemberain.com>
2011-08-30 01:09:42 +0400

    Add leiningen plugin for marginalia to development dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
   :license { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0-beta2"]
                  [com.rabbitmq/amqp-client "2.5.1"]]
+  :dev-dependencies [[lein-marginalia "0.6.0"]]
   :warn-on-reflection true
   :jvm-opts ["-Xmx512m"])
