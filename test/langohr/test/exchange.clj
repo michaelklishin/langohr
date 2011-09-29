26fe96e61ab6121a5d8e1d2542ebcc6edbed0978
Michael S. Klishin <michael@novemberain.com>
2011-09-29 13:46:28 +0400

    Started replacing horrible initial tests
     * The only meaningful kind of tests for exchange.declare I can imagine is to route
       some messages through newly created exchanges.

--- test/langohr/test/exchange.clj
+++ test/langohr/test/exchange.clj
@@ -14,74 +14,65 @@
 ;; exchange.declare
 ;;
 
-(defonce ^:dynamic ^Connection *conn* (lhc/connect))
+(defonce ^:dynamic ^Connection conn (lhc/connect))
 
 
 (deftest t-declare-a-direct-exchange-with-default-attributes
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.direct1"
-        declare-ok (lhe/declare channel exchange "direct")]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.direct1"]
+    (lhe/declare channel exchange "direct")))
 
 
 (deftest t-declare-a-durable-direct-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.direct2"
-        declare-ok (lhe/declare channel exchange "direct" :auto-delete false, :durable true)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.direct2"]
+    (lhe/declare channel exchange "direct" :auto-delete false, :durable true)))
 
 
 (deftest t-declare-an-auto-deleted-direct-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.direct3"
-        declare-ok (lhe/declare channel exchange "direct" :auto-delete true, :durable false)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.direct3"]
+    (lhe/declare channel exchange "direct" :auto-delete true, :durable false)))
 
 
 
 
 (deftest t-declare-a-fanout-exchange-with-default-attributes
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.fanout1"
-        declare-ok (lhe/declare channel exchange "fanout")]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.fanout1"]
+    (lhe/declare channel exchange "fanout")))
 
 
 (deftest t-declare-a-durable-fanout-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.fanout2"
-        declare-ok (lhe/declare channel exchange "fanout" :durable true)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.fanout2"]
+    (lhe/declare channel exchange "fanout" :durable true)))
 
 
 (deftest t-declare-an-auto-deleted-fanout-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.fanout3"
-        declare-ok (lhe/declare channel exchange "fanout" :auto-delete true)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.fanout3"]
+    (lhe/declare channel exchange "fanout" :auto-delete true)))
 
 
 
 
 (deftest t-declare-a-topic-exchange-with-default-attributes
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.topic1"
-        declare-ok (lhe/declare channel exchange "topic")]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.topic1"]
+    (lhe/declare channel exchange "topic")))
 
 
 (deftest t-declare-a-durable-topic-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.topic2"
-        declare-ok (lhe/declare channel exchange "topic" :durable true)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.topic2"]
+    (lhe/declare channel exchange "topic" :durable true)))
 
 
 (deftest t-declare-an-auto-deleted-topic-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.topic3"
-        declare-ok (lhe/declare channel exchange "topic" :auto-delete true)]
-    (is (instance? AMQP$Exchange$DeclareOk declare-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.topic3"]
+    (lhe/declare channel exchange "topic" :auto-delete true)))
 
 
 
@@ -90,18 +81,16 @@
 ;;
 
 (deftest t-delete-a-fresh-declared-direct-exchange
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.direct4"
-        declare-ok (lhe/declare channel exchange "direct")
-        delete-ok  (lhe/delete  channel exchange)]
-    (is (instance? AMQP$Exchange$DeleteOk delete-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.direct4"]
+    (lhe/declare channel exchange "direct")
+    (lhe/delete  channel exchange)))
 
 (deftest t-delete-a-fresh-declared-direct-exchange-if-it-is-unused
-  (let [^Channel   channel    (lhc/create-channel *conn*)
-        ^String    exchange   "langohr.tests.exchanges.direct5"
-        declare-ok (lhe/declare channel exchange "direct")
-        delete-ok  (lhe/delete  channel exchange true)]
-    (is (instance? AMQP$Exchange$DeleteOk delete-ok))))
+  (let [channel    (lhc/create-channel conn)
+        exchange   "langohr.tests.exchanges.direct5"]
+    (lhe/declare channel exchange "direct")
+    (lhe/delete  channel exchange true)))
 
 
 
@@ -110,7 +99,7 @@
 ;;
 
 (deftest t-exchange-bind-without-arguments
-  (let [channel     (lhc/create-channel *conn*)
+  (let [channel     (lhc/create-channel conn)
         source      "langohr.tests.exchanges.source"
         destination "langohr.tests.exchanges.destination"
         queue       (.getQueue (lhq/declare channel "" :auto-delete true))]
@@ -123,7 +112,7 @@
     (is (lhb/get channel queue))))
 
 (deftest t-exchange-bind-with-arguments
-  (let [channel     (lhc/create-channel *conn*)
+  (let [channel     (lhc/create-channel conn)
         source      "langohr.tests.exchanges.source2"
         destination "langohr.tests.exchanges.destination2"
         queue       (.getQueue (lhq/declare channel "" :auto-delete true))]
