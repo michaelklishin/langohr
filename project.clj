a3d26ed55aaed783476dd164955e28fdfedbbf77
Michael S. Klishin <michael@novemberain.com>
2011-10-04 12:05:39 +0400

    Upgrade to the RabbitMQ Java client 2.6.1
     * Apparently it was released to the OSS Sonatype repo, just not to the Maven Central

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :description "An experimental Clojure layer on top of the RabbitMQ Java client"
   :url         "https://github.com/michaelklishin/langohr"
   :license     { :name "Eclipse Public License" }
+  :repositories { "sonatype"
+                 {:url "http://oss.sonatype.org/content/repositories/releases"
+                  :snapshots false
+                  :releases {:checksum :fail :update :always}
+                  }}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.rabbitmq/amqp-client "2.5.1"]]
+                 [com.rabbitmq/amqp-client "2.6.1"]]
   :warn-on-reflection true
   :jvm-opts ["-Xmx512m"])
