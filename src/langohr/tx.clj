dd5d3df3fc0a9916a6f39ef7a74c445ce01cbc70
Michael S. Klishin <michael@novemberain.com>
2011-10-09 02:38:14 +0400

    Remove unnecessarity import

--- src/langohr/tx.clj
+++ src/langohr/tx.clj
@@ -8,7 +8,7 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns langohr.tx
-  (:import (com.rabbitmq.client Channel AMQP$Tx$SelectOk)))
+  (:import (com.rabbitmq.client Channel)))
 
 
 ;;
