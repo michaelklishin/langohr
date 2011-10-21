ad4e34e694e7ee1cd6964ae11c3abb32b9da1a6c
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-10-21 14:16:46 +0200

    Turning examples to simple/sample CLI tools.

--- src/langohr/examples/basic_consume.clj
+++ src/langohr/examples/basic_consume.clj
@@ -1,21 +1,45 @@
 (ns langohr.examples.basic-consume
-  (:import (java.security SecureRandom) (java.math.BigInteger))
-  (:use [langohr.core :only [connect close]] [langohr.basic :only [consume]]))
+  "Basic-consume CLI. Run it with:
+
+       lein exec src/langohr/examples/basic_consume.clj
+
+  "
+  (:import (java.security SecureRandom)
+           (java.math.BigInteger)
+           (com.rabbitmq.client Connection))
+  (require [langohr.consumers :as lhcons])
+  (:use [langohr.core :only [connect close]]
+        [langohr.basic :only [consume]]
+        [clojure.tools.cli]))
 
 (declare message-handler)
 
-(defn default
+(defn consume-one
   [queue]
   (let [conn         (connect { :username "langohr", :password "langohr", :vhost "langohr.dev" })
-        ch           (.createChannel conn)
-        consumer-tag (.toString (new BigInteger 130 (SecureRandom.)) 32)]
+        channel      (.createChannel conn)
+        consumer-tag (.toString (new BigInteger 130 (SecureRandom.)) 32)
+        consumer     (lhcons/create-default channel :handle-delivery-fn message-handler)
+        ]
     (do
       (println (str "Going to consume messages from  " queue))
       (println (str "Consumer tag is " consumer-tag))
-      (consume ch queue message-handler { :consumer-tag consumer-tag, :auto-ack true })
-      (close ch)
+      (consume channel queue consumer :consumer-tag consumer-tag, :auto-ack true)
+      (close channel)
       (close conn))))
 
 (defn message-handler
   [delivery message-properties message-payload]
   (println (str "Got a message: " (String. message-payload) ", headers: " (str (.getHeaders message-properties)))))
+
+(do
+  []
+  (let [
+        args   *command-line-args*
+        parsed-args (cli args
+                         (optional ["--username" "Username" :default "langohr"])
+                         (optional ["--password" "Password" :default "langohr"])
+                         (optional ["--vhost" "RabbitMQ Virtual Host" :default "langohr.dev"])
+                         (required ["--queue-name" "Queue name"])) ]
+
+  (consume-one (:queue-name parsed-args))))
--- src/langohr/examples/queue_declare.clj
+++ src/langohr/examples/queue_declare.clj
@@ -1,28 +1,52 @@
 (ns langohr.examples.queue-declare
-  (:import (com.rabbitmq.client Channel))
-  (:use [langohr.core :only [connect close]])
-  (:require langohr.queue))
+  "Queue declare CLI. Run it with:
+
+       lein exec src/langohr/examples/queue_declare.clj
+
+  "
+  (:import (com.rabbitmq.client Channel Connection AMQP$Queue$DeclareOk))
+  (:use [langohr.core :only [connect close]]
+        [langohr.queue :as lhc]
+        [clojure.tools.cli]))
+
+
+(def ^:dynamic *langohr-settings*)
 
 (defn client-named
-  [name]
-  (let [conn       (connect { :username "langohr", :password "langohr", :vhost "langohr.dev" })
-        ch         (.createChannel conn)
-        declare-ok (langohr.queue/declare ch name { :durable true, :auto-delete false })]
+  [queue-name]
+  (let [conn       (connect { :username (:username *langohr-settings*) :password (:password *langohr-settings*) :vhost (:vhost *langohr-settings*) })
+        channel    (.createChannel conn)
+        declare-ok (lhc/declare channel queue-name :durable true :exclusive false :auto-delete false)]
     (do
-      (println (str "Going to declare a queue named " name))
-      (println (str "Declared, messages count: " (.getMessageCount declare-ok)))
-      ;;(close ch)
-      ;;(close conn)
+      (println (str "Going to declare a queue named " queue-name))
+      (println (str "Declared: " (.getQueue declare-ok)))
+      (println (str "Messages count: " (.getMessageCount declare-ok)))
+      (close channel)
+      (close conn)
       )))
 
 
 (defn server-named
   []
   (let [conn        (connect { :username "langohr", :password "langohr", :vhost "langohr.dev" })
-        ch          (.createChannel conn)
-        declare-ok  (langohr.queue/declare ch)]
+        channel     (.createChannel conn)
+        declare-ok  (lhc/declare channel)]
     (do
       (println "Going to declare a server-named queue")
       (println (str "Declared: " (.getQueue declare-ok)))
-      (close ch)
+      (close channel)
       (close conn))))
+
+(do
+  (let [
+        args        *command-line-args*
+        parsed-args (cli args
+                         (optional ["--username" "Username" :default "langohr"])
+                         (optional ["--password" "Password" :default "langohr"])
+                         (optional ["--vhost" "RabbitMQ Virtual Host" :default "langohr.dev"])
+                         (optional ["--queue-name" "Queue name"])) ]
+    (def ^:dynamic *langohr-settings* parsed-args)
+    (let [queue-name (:queue-name parsed-args) ]
+      (if (nil? queue-name)
+        (server-named)
+        (client-named queue-name)))))
