62614ed3a28d2535df665001d40c91c4b8ed08df
Michael S. Klishin <michael@novemberain.com>
2012-09-06 03:13:19 +0400

    Add Support dependency
    [ci skip]

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure      "1.3.0"]
-                 [com.rabbitmq/amqp-client "2.8.6"]]
+                 [com.rabbitmq/amqp-client "2.8.6"]
+                 [clojurewerkz/support     "0.7.0-beta2"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]] }
              :1.4 { :dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
