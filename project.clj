3da03fabfe8e41526b4d78497b3c7c708adb6687
Michael Klishin <michael@defprotocol.org>
2013-03-17 19:18:36 +0400

    No longer need to override Guava version

--- project.clj
+++ project.clj
@@ -11,8 +11,7 @@
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]
-                                  [com.google.guava/guava "14.0"]]
+             :dev {:dependencies [[org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["test/resources"]
                    :plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
