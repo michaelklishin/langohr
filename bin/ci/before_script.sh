b2b0e6b8df31203742bbc0737928eca7d34c95e8
Michael Klishin <michael@defprotocol.org>
2013-05-16 00:03:24 +0400

    Allow overriding rabbitmqctl location via RABBITMQCTL

--- bin/ci/before_script.sh
+++ bin/ci/before_script.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+${RABBITMQCTL:="sudo rabbitmqctl"}
+${RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+
 # guest:guest has full access to /
 
-sudo rabbitmqctl add_vhost /
-sudo rabbitmqctl add_user guest guest
-sudo rabbitmqctl set_permissions -p / guest ".*" ".*" ".*"
+$RABBITMQCTL add_vhost /
+$RABBITMQCTL add_user guest guest
+$RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
 
 
 # langohr:langohr.password has full access to / and langohr_testbed
 
-sudo rabbitmqctl add_vhost langohr_testbed
-sudo rabbitmqctl add_user langohr "langohr.password"
-sudo rabbitmqctl set_permissions -p /               langohr ".*" ".*" ".*"
-sudo rabbitmqctl set_permissions -p langohr_testbed langohr ".*" ".*" ".*"
+$RABBITMQCTL add_vhost langohr_testbed
+$RABBITMQCTL add_user langohr "langohr.password"
+$RABBITMQCTL set_permissions -p /               langohr ".*" ".*" ".*"
+$RABBITMQCTL set_permissions -p langohr_testbed langohr ".*" ".*" ".*"
 
-sudo rabbitmqctl set_permissions -p /               guest ".*" ".*" ".*"
-sudo rabbitmqctl set_permissions -p langohr_testbed guest ".*" ".*" ".*"
+$RABBITMQCTL set_permissions -p /               guest ".*" ".*" ".*"
+$RABBITMQCTL set_permissions -p langohr_testbed guest ".*" ".*" ".*"
 
 sleep 3
