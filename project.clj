2489275f37a75b0ac9d2463096ec9bed1dbe802f
Michael Klishin <michael@defprotocol.org>
2013-07-27 00:04:56 +0400

    clj-http 0.7.4

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure      "1.5.1"]
                  [com.rabbitmq/amqp-client "3.1.3"]
                  [clojurewerkz/support     "0.15.0"]
-                 [clj-http                 "0.6.4"]
+                 [clj-http                 "0.7.4"]
                  [cheshire                 "5.0.2"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
