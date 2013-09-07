d4d47a22b1826afe1a5e14f569a7c01489b84dc9
Michael Klishin <michael@defprotocol.org>
2013-09-07 10:39:36 +0100

    Drop Clojure 1.3 support

--- project.clj
+++ project.clj
@@ -7,8 +7,7 @@
                  [clojurewerkz/support     "0.16.0"]
                  [clj-http                 "0.7.6"]
                  [cheshire                 "5.2.0"]]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]]
@@ -26,7 +25,7 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
   :warn-on-reflection true
   :jvm-opts ["-Xmx512m"]
   :test-selectors {:default        (fn [m]
