7d40c632ebfd136c053df04a7cc02324b3158562
Michael Klishin <michael@clojurewerkz.org>
2013-10-08 21:37:26 +0400

    Bump clj-http

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure      "1.5.1"]
                  [com.rabbitmq/amqp-client "3.1.4"]
                  [clojurewerkz/support     "0.20.0"]
-                 [clj-http                 "0.7.6"]
+                 [clj-http                 "0.7.7"]
                  [cheshire                 "5.2.0"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
