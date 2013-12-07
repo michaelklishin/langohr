7a9357a0bdb72e246f9b2cf723d45b68025f9af6
Michael Klishin <michael@clojurewerkz.org>
2013-12-07 16:15:33 +0400

    Ditto

--- test/langohr/test/shutdown_test.clj
+++ test/langohr/test/shutdown_test.clj
@@ -10,94 +10,97 @@
            java.util.concurrent.TimeUnit))
 
 
-(defonce ^Connection conn (lhc/connect))
-
 (deftest test-channel-of-with-a-channel-exception
-  (let [ch    (lch/open conn)
-        q     (:queue (lhq/declare ch))
-        latch    (java.util.concurrent.CountDownLatch. 1)
-        cha      (atom nil)
-        f        (fn [consumer_tag ^ShutdownSignalException reason]
-                   (reset! cha (lh/channel-of reason))
-                   (.countDown latch))
-        consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
-    (lhb/consume ch q consumer)
-    (.start (Thread. (fn []
-                       (try
-                         (lhq/bind ch "ugggggh" "amq.fanout")
-                         (catch Exception e
-                           (comment "Do nothing"))))))
-    (is (.await latch 700 TimeUnit/MILLISECONDS))
-    (is (= @cha (.getDelegate ch)))))
+  (with-open [^Connection conn (lhc/connect)]
+    (let [ch    (lch/open conn)
+          q     (:queue (lhq/declare ch))
+          latch    (java.util.concurrent.CountDownLatch. 1)
+          cha      (atom nil)
+          f        (fn [consumer_tag ^ShutdownSignalException reason]
+                     (reset! cha (lh/channel-of reason))
+                     (.countDown latch))
+          consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
+      (lhb/consume ch q consumer)
+      (.start (Thread. (fn []
+                         (try
+                           (lhq/bind ch "ugggggh" "amq.fanout")
+                           (catch Exception e
+                             (comment "Do nothing"))))))
+      (is (.await latch 700 TimeUnit/MILLISECONDS))
+      (is (= @cha (.getDelegate ch))))))
 
 (deftest test-connection-of
-  (let [ch    (lch/open conn)
-        q     (:queue (lhq/declare ch))
-        latch    (java.util.concurrent.CountDownLatch. 1)
-        conn'    (atom nil)
-        f        (fn [consumer_tag ^ShutdownSignalException reason]
-                   (reset! conn' (lh/connection-of reason))
-                   (.countDown latch))
-        consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
-    (lhb/consume ch q consumer)
-    (.start (Thread. (fn []
-                       (try
-                         (lhq/bind ch "ugggggh" "amq.fanout")
-                         (catch Exception e
-                           (comment "Do nothing"))))))
-    (is (.await latch 700 TimeUnit/MILLISECONDS))
-    (is (= @conn' (.getDelegate conn)))))
+  (with-open [^Connection conn (lhc/connect)]
+    (let [ch    (lch/open conn)
+          q     (:queue (lhq/declare ch))
+          latch    (java.util.concurrent.CountDownLatch. 1)
+          conn'    (atom nil)
+          f        (fn [consumer_tag ^ShutdownSignalException reason]
+                     (reset! conn' (lh/connection-of reason))
+                     (.countDown latch))
+          consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
+      (lhb/consume ch q consumer)
+      (.start (Thread. (fn []
+                         (try
+                           (lhq/bind ch "ugggggh" "amq.fanout")
+                           (catch Exception e
+                             (comment "Do nothing"))))))
+      (is (.await latch 700 TimeUnit/MILLISECONDS))
+      (is (= @conn' (.getDelegate conn))))))
 
 (deftest test-initiator-with-a-channel-exception
-  (let [ch    (lch/open conn)
-        q     (:queue (lhq/declare ch))
-        latch    (java.util.concurrent.CountDownLatch. 1)
-        sse      (atom nil)
-        f        (fn [consumer_tag ^ShutdownSignalException reason]
-                   (reset! sse reason)
-                   (.countDown latch))
-        consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
-    (lhb/consume ch q consumer)
-    (.start (Thread. (fn []
-                       (try
-                         (lhq/bind ch "ugggggh" "amq.fanout")
-                         (catch Exception e
-                           (comment "Do nothing"))))))
-    (is (.await latch 700 TimeUnit/MILLISECONDS))
-    (is (lh/initiated-by-broker? @sse))
-    (is (not (lh/initiated-by-application? @sse)))))
+  (with-open [^Connection conn (lhc/connect)]
+    (let [ch    (lch/open conn)
+          q     (:queue (lhq/declare ch))
+          latch    (java.util.concurrent.CountDownLatch. 1)
+          sse      (atom nil)
+          f        (fn [consumer_tag ^ShutdownSignalException reason]
+                     (reset! sse reason)
+                     (.countDown latch))
+          consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
+      (lhb/consume ch q consumer)
+      (.start (Thread. (fn []
+                         (try
+                           (lhq/bind ch "ugggggh" "amq.fanout")
+                           (catch Exception e
+                             (comment "Do nothing"))))))
+      (is (.await latch 700 TimeUnit/MILLISECONDS))
+      (is (lh/initiated-by-broker? @sse))
+      (is (not (lh/initiated-by-application? @sse))))))
 
 (deftest test-initiator-with-an-explicit-channel-closure
-  (let [ch    (lch/open conn)
-        q     (:queue (lhq/declare ch))
-        latch    (java.util.concurrent.CountDownLatch. 1)
-        sse      (atom nil)
-        f        (fn [consumer_tag ^ShutdownSignalException reason]
-                   (reset! sse reason)
-                   (.countDown latch))
-        consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
-    (lhb/consume ch q consumer)
-    (Thread/sleep 250)
-    (lhc/close ch)
-    (is (.await latch 700 TimeUnit/MILLISECONDS))
-    (is (not (lh/initiated-by-broker? @sse)))
-    (is (lh/initiated-by-application? @sse))))
+  (with-open [^Connection conn (lhc/connect)]
+    (let [ch    (lch/open conn)
+          q     (:queue (lhq/declare ch))
+          latch    (java.util.concurrent.CountDownLatch. 1)
+          sse      (atom nil)
+          f        (fn [consumer_tag ^ShutdownSignalException reason]
+                     (reset! sse reason)
+                     (.countDown latch))
+          consumer (lhcons/create-default ch :handle-shutdown-signal-fn f)]
+      (lhb/consume ch q consumer)
+      (Thread/sleep 250)
+      (lhc/close ch)
+      (is (.await latch 700 TimeUnit/MILLISECONDS))
+      (is (not (lh/initiated-by-broker? @sse)))
+      (is (lh/initiated-by-application? @sse)))))
 
 (deftest test-initiator-with-an-unhandled-consumer-exception
-  (let [ch    (lch/open conn)
-        q     (:queue (lhq/declare ch))
-        latch    (java.util.concurrent.CountDownLatch. 1)
-        sse      (atom nil)
-        dhf      (fn [ch metadata payload]
-                   ;; something terrible happens
-                   (throw (RuntimeException. "the monster, it is out! Run for life!")))
-        ssf      (fn [consumer_tag ^ShutdownSignalException reason]
-                   (reset! sse reason)
-                   (.countDown latch))
-        consumer (lhcons/create-default ch :handle-delivery-fn dhf :handle-shutdown-signal-fn ssf)]
-    (lhb/consume ch q consumer)
-    (Thread/sleep 250)
-    (lhb/publish ch "" q "message")
-    (is (.await latch 700 TimeUnit/MILLISECONDS))
-    (is (not (lh/initiated-by-broker? @sse)))
-    (is (lh/initiated-by-application? @sse))))
+  (with-open [^Connection conn (lhc/connect)]
+    (let [ch    (lch/open conn)
+          q     (:queue (lhq/declare ch))
+          latch    (java.util.concurrent.CountDownLatch. 1)
+          sse      (atom nil)
+          dhf      (fn [ch metadata payload]
+                     ;; something terrible happens
+                     (throw (RuntimeException. "the monster, it is out! Run for life!")))
+          ssf      (fn [consumer_tag ^ShutdownSignalException reason]
+                     (reset! sse reason)
+                     (.countDown latch))
+          consumer (lhcons/create-default ch :handle-delivery-fn dhf :handle-shutdown-signal-fn ssf)]
+      (lhb/consume ch q consumer)
+      (Thread/sleep 250)
+      (lhb/publish ch "" q "message")
+      (is (.await latch 700 TimeUnit/MILLISECONDS))
+      (is (not (lh/initiated-by-broker? @sse)))
+      (is (lh/initiated-by-application? @sse)))))
