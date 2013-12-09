d9edcee184d1ca73fb2041badb37381aed24300f
Michael Klishin <michael@clojurewerkz.org>
2013-12-09 17:04:47 +0400

    One more recovery test

--- test/langohr/test/recovery_test.clj
+++ test/langohr/test/recovery_test.clj
@@ -45,3 +45,19 @@
     (is (rmq/open? ch1))
     (is (rmq/open? ch2))
     (rmq/close conn)))
+
+(deftest test-basic-client-named-queue-recovery
+  (let [conn (rmq/connect {:automatically-recover true
+                           :automatically-recover-topology true
+                           :network-recovery-delay 500})
+        ch   (lch/open conn)
+        q    "langohr.test.recovery.q1"]
+    (lq/declare ch q :durable true)
+    (lq/purge ch q)
+    (is (lq/empty? ch q))
+    (close-all-connections)
+    (Thread/sleep 1000)
+    (is (rmq/open? ch))
+    (lb/publish ch "" q "a message")
+    (is (not (lq/empty? ch q)))
+    (rmq/close conn)))
