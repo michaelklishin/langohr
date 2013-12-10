f5bf8b1ec63bc95e340928f6c2853c8e8fdb04f3
Michael Klishin <michael@clojurewerkz.org>
2013-12-10 12:30:33 +0400

    Optimize imports

--- src/java/com/novemberain/langohr/Channel.java
+++ src/java/com/novemberain/langohr/Channel.java
@@ -5,7 +5,9 @@ import com.novemberain.langohr.recovery.*;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeoutException;
 
