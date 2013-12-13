98b0166b10a5eed0521ccd1c37adc153193eaef3
Michael Klishin <michael@clojurewerkz.org>
2013-12-13 15:59:27 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/langohr "2.0.0-rc4"
+(defproject com.novemberain/langohr "2.0.0-rc5-SNAPSHOT"
   :description "An idiomatic Clojure client for RabbitMQ that embraces AMQP 0.9.1 model. Built on top of the RabbitMQ Java client"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
--- src/clojure/langohr/core.clj
+++ src/clojure/langohr/core.clj
@@ -64,7 +64,7 @@
 
 
 (def ^{:const true}
-  version "2.0.0-rc4")
+  version "2.0.0-rc5-SNAPSHOT")
 
 (declare create-connection-factory)
 (defn ^Connection connect
