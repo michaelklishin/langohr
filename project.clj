5b673c21d1b6a6e6e4a2a192754fc2cd62e364e2
Michael Klishin <michael@clojurewerkz.org>
2014-01-12 18:40:25 +0400

    tools.cli 0.3.1

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[org.clojure/tools.cli "0.2.1" :exclusions [org.clojure/clojure]]]
+             :dev {:dependencies [[org.clojure/tools.cli "0.3.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["test/resources"]
                    :plugins [[codox "0.6.6"]]
                    :codox {:sources ["src/clojure"]
