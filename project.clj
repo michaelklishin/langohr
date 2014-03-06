d39d50649e8184d67db2327e5388fc727cd934ae
Michael Klishin <michael@clojurewerkz.org>
2014-03-06 16:30:43 +0400

    Test against clj-http 0.7.6 in a separate profile
    Makes sure we are compatible (when clj-http dependency is excluded)
    with projects that rely on HTTPCore 4.2.x, e.g. those using
    Spring 3.

--- project.clj
+++ project.clj
@@ -10,6 +10,10 @@
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             ;; this version of clj-http depends on HTTPCore 4.2.x which
+             ;; some projects (e.g. using Spring's RestTemplate) can rely on,
+             ;; so we test for compatibility with it. MK.
+             :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
              :dev {:dependencies [[org.clojure/tools.cli "0.3.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["test/resources"]
                    :plugins [[codox "0.6.6"]]
@@ -25,7 +29,7 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master:dev,cljhttp076:dev,1.6,cljhttp076"]}
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-Xmx512m"]
   :test-selectors {:default        (fn [m]
