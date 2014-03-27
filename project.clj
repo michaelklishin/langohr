111c352b365f6a2b57f7b80401a345f7dbb46d8f
Michael Klishin <michael@clojurewerkz.org>
2014-03-27 09:27:01 +0400

    master is now 1.7.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [cheshire                 "5.3.1"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              ;; this version of clj-http depends on HTTPCore 4.2.x which
              ;; some projects (e.g. using Spring's RestTemplate) can rely on,
              ;; so we test for compatibility with it. MK.
