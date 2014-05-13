48e5aa694e7ffd58e4fb5851bed0185fd8d54e6f
Michael Klishin <michael@clojurewerkz.org>
2014-05-13 16:09:34 +0400

    Depend on RabbitMQ Java client 3.3.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure      "1.6.0"]
-                 [com.rabbitmq/amqp-client "3.3.0"]
+                 [com.rabbitmq/amqp-client "3.3.1"]
                  [clojurewerkz/support     "0.20.0"]
                  [clj-http                 "0.9.1"]
                  [cheshire                 "5.3.1"]]
