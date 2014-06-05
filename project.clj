ea3277219dc02a757bf85f49e442ec735e67748f
Michael Klishin <michael@clojurewerkz.org>
2014-06-05 21:53:35 +0400

    Codox 0.8.9

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
              :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
              :dev {:dependencies [[org.clojure/tools.cli "0.3.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["test/resources"]
-                   :plugins [[codox "0.8.8"]]
+                   :plugins [[codox "0.8.9"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
   :source-paths      ["src/clojure"]
