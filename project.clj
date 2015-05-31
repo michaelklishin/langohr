37a75fc63eb6787ae6509b18fe71aeb80f7719d6
Michael Klishin <michael@clojurewerkz.org>
2015-05-31 17:39:46 +0300

    Test against Clojure 1.7.0-RC1

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [clojurewerkz/support     "1.1.0" :exclusions [com.google.guava/guava]]
                  [clj-http                 "1.1.1"]
                  [cheshire                 "5.4.0"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              ;; this version of clj-http depends on HTTPCore 4.2.x which
              ;; some projects (e.g. using Spring's RestTemplate) can rely on,
