db3bd64f75a2592329989356918ff7e3a22f7680
Michael Klishin <michael@clojurewerkz.org>
2015-12-20 01:53:30 +0300

    Bump Codox

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
              :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}
              :dev {:dependencies [[org.clojure/tools.cli "0.3.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["test/resources"]
-                   :plugins [[codox "0.8.10"]]
+                   :plugins [[codox "0.9.0"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
   :source-paths      ["src/clojure"]
