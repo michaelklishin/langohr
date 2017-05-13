8443000d12a1d4a23e6e8f2e5efa4088ab31fc29
Michael Klishin <michael@clojurewerkz.org>
2017-05-13 23:19:04 +0300

    Use Java client 4.1.0 GA

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.5.1"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure      "1.8.0"]
-                 [com.rabbitmq/amqp-client "4.0.2"]
+                 [com.rabbitmq/amqp-client "4.1.0"]
                  [clojurewerkz/support     "1.1.0" :exclusions [com.google.guava/guava]]
                  [clj-http                 "3.4.1"]
                  [cheshire                 "5.6.3"]]
