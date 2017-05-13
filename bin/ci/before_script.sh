0f096835d06a5c8594ca108a7bbd6c32af852208
Michael Klishin <michael@clojurewerkz.org>
2017-05-13 23:28:21 +0300

    Adapt Travis for 3.6.7+ RabbitMQ management plugin versions
    The same way we did for Hop, Rabbithole, and other HTTP API clients.

--- bin/ci/before_script.sh
+++ bin/ci/before_script.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
-${LANGOHR_RABBITMQCTL:="sudo rabbitmqctl"}
-${LANGOHR_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+CTL=${LANGOHR_RABBITMQCTL:="sudo rabbitmqctl"}
+PLUGINS=${LANGOHR_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+
+$PLUGINS enable rabbitmq_management
+
+sleep 3
 
 # guest:guest has full access to /
 
-$LANGOHR_RABBITMQCTL add_vhost /
-$LANGOHR_RABBITMQCTL add_user guest guest
-$LANGOHR_RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
+$CTL add_vhost /
+$CTL add_user guest guest
+$CTL set_permissions -p / guest ".*" ".*" ".*"
 
+# Reduce retention policy for faster publishing of stats
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
 
 # langohr:langohr.password has full access to / and langohr_testbed
 
-$LANGOHR_RABBITMQCTL add_vhost langohr_testbed
-$LANGOHR_RABBITMQCTL add_user langohr "langohr.password"
-$LANGOHR_RABBITMQCTL set_permissions -p /               langohr ".*" ".*" ".*"
-$LANGOHR_RABBITMQCTL set_permissions -p langohr_testbed langohr ".*" ".*" ".*"
+$CTL add_vhost langohr_testbed
+$CTL add_user langohr "langohr.password"
+$CTL set_permissions -p /               langohr ".*" ".*" ".*"
+$CTL set_permissions -p langohr_testbed langohr ".*" ".*" ".*"
 
-$LANGOHR_RABBITMQCTL set_permissions -p /               guest ".*" ".*" ".*"
-$LANGOHR_RABBITMQCTL set_permissions -p langohr_testbed guest ".*" ".*" ".*"
-
-$LANGOHR_RABBITMQ_PLUGINS enable rabbitmq_management
+$CTL set_permissions -p /               guest ".*" ".*" ".*"
+$CTL set_permissions -p langohr_testbed guest ".*" ".*" ".*"
 
 sleep 3
