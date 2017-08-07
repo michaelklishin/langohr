89c0bce471fc7df59349c11919eba5ea273bd6e2
Michael Klishin <michael@novemberain.com>
2017-08-07 17:22:11 +0300

    Update install_on_debian.sh

--- bin/ci/install_on_debian.sh
+++ bin/ci/install_on_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /tmp/ || exit 1
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_9/rabbitmq-server_3.6.10-1_all.deb
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_10/rabbitmq-server_3.6.10-1_all.deb
 sudo dpkg --install rabbitmq-server_3.6.10-1_all.deb
 sudo rm rabbitmq-server_3.6.10-1_all.deb
 
