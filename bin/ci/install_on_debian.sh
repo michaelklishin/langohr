7f38a2d611e7bfb9c44d5fd5dcfaeaea5fcd496a
Michael Klishin <michael@novemberain.com>
2017-08-07 17:47:28 +0300

    Install package dependencies since we use dpkg

--- bin/ci/install_on_debian.sh
+++ bin/ci/install_on_debian.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo apt-get update -y
+sudo apt-get install -y init-system-helpers socat adduser logrotate
+
 cd /tmp/ || exit 1
 wget https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_10/rabbitmq-server_3.6.10-1_all.deb
 sudo dpkg --install rabbitmq-server_3.6.10-1_all.deb
