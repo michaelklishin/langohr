b43cbeaec9325aa26890ee86703f010d5bb48410
Michael Klishin <michael@clojurewerkz.org>
2018-03-21 00:59:40 +0300

    5.0.0

--- src/clojure/langohr/core.clj
+++ src/clojure/langohr/core.clj
@@ -305,8 +305,8 @@
                      "information"  "See http://clojurerabbitmq.info/"
                      "platform"     (platform-string)
                      "capabilities" (get (AMQConnection/defaultClientProperties) "capabilities")
-                     "copyright"    "Copyright (C) 2011-2017 Michael S. Klishin, Alex Petrov"
-                     "version"      "4.1.0-SNAPSHOT"})
+                     "copyright"    "Copyright (C) 2011-2018 Michael S. Klishin, Alex Petrov"
+                     "version"      "5.0.0"})
 
 (defn- auth-mechanism->sasl-config
   [{:keys [authentication-mechanism]}]
